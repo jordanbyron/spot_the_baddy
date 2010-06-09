165aa5d2172b6819ed8d06babfbe7882604624ab
Jordan Byron <jordan@ducksoupsoftware.com>
2010-06-08 23:10:54 -0400

    Added jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,39 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function enableEdit(){
+  $('#time_entries .row:not(.edit)').live('dblclick', function(e){
+    $(e.currentTarget).addClass('edit').find('.nubbin').addClass('edit');
+    $('#loading').show();
+    
+    
+    var id = $(e.currentTarget).attr('id').replace(/time_entry_/, '');
+    $.get('/time_entries/' + id + '/edit');
+  });
+}
+
+function enableNubbins(){
+  $('#time_entries .row').live('mouseover', function(e){
+    $('.nubbin:not(.edit)').each(function(id, nub){
+      $(nub).hide();
+		});
+
+    $(e.currentTarget).find('.nubbin').show();
+    
+  });
+	
+  $('#time_entries').mouseout(function(){
+    $('.nubbin:not(.edit)').each(function(id, nub){
+      $(nub).hide();
+		});
+	});
+}
+
+function updateTotalHours(change){
+  var totalHours = 0.0;
+  
+  $('.time_entry .hours').each(function(id, data){
+    if($(data).text().length > 0)
+      totalHours += parseFloat($(data).text());
+  });
+  
+  totalHours = Math.round(totalHours*100)/100
+  $("#total_hours").text(totalHours);
+}
